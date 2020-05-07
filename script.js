9cfba0930e67da3f4124719fbdc3127c221d3e0f
Eileen Reid <eileenreid4657@gmail.com>
2020-05-07 13:59:27 -0400

    Initializing

---
+++ script.js
@@ -0,0 +1,22 @@
+var statusSpan = document.querySelector("#status");
+var statusToggle = document.querySelector("#status-toggle");
+var playButton = document.querySelector("#play");
+var pauseButton = document.querySelector("#pause");
+var stopButton = document.querySelector("#stop");
+var minutesDisplay = document.querySelector("#minutes");
+var secondsDisplay = document.querySelector("#seconds");
+var workMinutesInput = document.querySelector("#work-minutes");
+var restMinutesInput = document.querySelector("#rest-minutes");
+
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var status = "Working";
+var interval;
+
+
+
+function startTimer() {
+  // Write code to start the timer here
+}
+
+playButton.addEventListener("click", startTimer);
